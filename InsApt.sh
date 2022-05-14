808b2bd8ed55b756b0f5a7fab632842faf5b1b2a
youseftaz <youseftaz5@gmail.com>
2022-05-14 15:09:11 +0200

    add

---
+++ InsApt.sh
@@ -0,0 +1,17 @@
+#i!/bin/sh
+
+yes | sudo apt-get install aptitide
+yes | sudo apt-get install clang
+yes | sudo apt-get tmux
+yes | sudo apt install build-essential #install g++ not gcc.
+yes | sudo apt-get install vim
+yes | sudo apt install tree
+yes | sudo apt install cmake
+yes | sudo apt install golang
+yes | sudo apt-get install mysql-server
+yes | sudo yarn install
+yes | sudo apt install ruby-full
+yes | sudo apt-get install software-properties-common
+yes | sudo apt-add-repository -y ppa:rael-gc/rvm
+yes | sudo apt-get update
+yes | sudo apt-get install rvm
